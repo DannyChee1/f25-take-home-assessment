f1ff2292e5b5767774184dbcdb67c5ab9ed7d8e7
DannyChee1 <officialjoebrabs@gmail.com>
2025-06-22 00:23:37 -0400

    Integrated WeatherStack API and added retrieved weather info into json.

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from typing import Dict, Any, Optional
 import uvicorn
 import uuid
+import requests
 
 app = FastAPI(title="Weather Data System", version="1.0.0")
 
@@ -28,8 +29,27 @@ class WeatherResponse(BaseModel):
 
 @app.post("/weather", response_model=WeatherResponse)
 async def create_weather_request(request: WeatherRequest):
+    response = requests.get(
+        "http://api.weatherstack.com/current",
+        params={
+            "access_key": "fb7d886fcfc63ff3ee17b045e0db60e7",
+            "query": request.location
+        },
+        timeout=10
+    )
+    if response.status_code != 200:
+        raise HTTPException(status_code=502, detail="Unintended WeatherStack behaviour")
+    
     unique_id = str(uuid.uuid1())
-    weather_storage[unique_id] = request.dict()
+    weather_storage[unique_id] = {
+        "id": unique_id,
+        "date": request.date,
+        "location": request.location,
+        "notes": request.notes,
+        "weather": response.json()["current"]
+    }
+
+    #weather_storage[unique_id] = request.dict()
     return WeatherResponse(id=unique_id)
     """
     You need to implement this endpoint to handle the following:
